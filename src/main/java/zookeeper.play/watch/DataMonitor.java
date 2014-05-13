9f31aa363f93ad46672ce991410d9785cf2bbf4b
James Baiera <james.baiera@gmail.com>
2014-05-13 15:26:00 -0400

    Simple watch client that runs a command every time a znode changes. Will kill the old command if znode changes during it's normal execution

---
+++ src/main/java/zookeeper.play/watch/DataMonitor.java
@@ -0,0 +1,144 @@
+package zookeeper.play.watch;
+
+import java.util.Arrays;
+
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.AsyncCallback.StatCallback;
+import org.apache.zookeeper.KeeperException.Code;
+import org.apache.zookeeper.data.Stat;
+
+/**
+ * A simple class that monitors the data and existence of a ZooKeeper
+ * node. It uses asynchronous ZooKeeper APIs.
+ *
+ * This will act somewhat like a View from MVC.
+ */
+public class DataMonitor implements Watcher, StatCallback {
+
+	private ZooKeeper zk;
+
+	private String znode;
+
+	private Watcher chainedWatcher;
+
+	private boolean dead;
+
+	private DataMonitorListener listener;
+
+	// The data that existed at the ZNode on the last call
+	private byte prevData[];
+
+	public DataMonitor(
+			ZooKeeper zk, String znode, Watcher chainedWatcher,
+			DataMonitorListener listener)
+	{
+		this.zk = zk;
+		this.znode = znode;
+		this.chainedWatcher = chainedWatcher;
+		this.listener = listener;
+		// Get things started by checking if the node exists. We are going
+		// to be completely event driven
+
+		// This object will be the stat call back but the executor will be the watcher
+		zk.exists(znode, true, this, null);
+	}
+
+	/**
+	 * Interface used for the Controller and Event processor for the data monitor "view"
+	 */
+	public interface DataMonitorListener {
+		/**
+		 * The existence status of the node has changed.
+		 */
+		void processExistsStatusChangeEvent(byte data[]);
+
+		/**
+		 * Zookeeper session is no longer valid, Close the program.
+		 *
+		 * @param rc the ZooKeeper reason code
+		 */
+		void closing(int rc);
+	}
+
+	@Override
+	public void process(WatchedEvent event) {
+		String path = event.getPath();
+		if (event.getType() == Event.EventType.None) {
+			// We are are being told that the state of the
+			// connection has changed
+			switch (event.getState()) {
+				case SyncConnected:
+					// In this particular example we don't need to do anything
+					// here - watches are automatically re-registered with
+					// server and any watches triggered while the client was
+					// disconnected will be delivered (in order of course)
+					break;
+				case Expired:
+					// Game over, man - GAME OVER
+					dead = true;
+					listener.closing(Code.SESSIONEXPIRED.intValue());
+					break;
+			}
+		} else {
+			if (path != null && path.equals(znode)) {
+				// Something has changed on the node, let's find out
+				zk.exists(znode, true, this, null);
+			}
+		}
+
+		// Optionally propagate to other watchers on this process
+		if (chainedWatcher != null) {
+			chainedWatcher.process(event);
+		}
+	}
+
+	@Override
+	public void processResult(int rc, String path, Object ctx, Stat stat) {
+		boolean exists;
+		switch (Code.get(rc)) {
+			case OK:
+				exists = true;
+				break;
+			case NONODE:
+				exists = false;
+				break;
+			case SESSIONEXPIRED:
+			case NOAUTH:
+				dead = true;
+				listener.closing(rc);
+				return;
+			default:
+				// Retry errors
+				zk.exists(znode, true, this, null);
+				return;
+		}
+
+		byte b[] = null;
+
+		if (exists) {
+			try {
+				b = zk.getData(znode, false, null);
+			} catch (KeeperException e) {
+				// We don't need to worry about recovering now. The watch
+				// callbacks will kick off any exception handling
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				return;
+			}
+		}
+
+		// If the data has changed since the last exists call, signal a change to the event listener (Executor)
+		if ((b == null && b != prevData) || (b != null && !Arrays.equals(prevData, b)))
+		{
+			listener.processExistsStatusChangeEvent(b);
+			prevData = b;
+		}
+	}
+
+	public boolean isDead() {
+		return dead;
+	}
+}
---
+++ src/main/java/zookeeper.play/watch/Executor.java
@@ -0,0 +1,192 @@
+package zookeeper.play.watch;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import com.sun.istack.internal.Nullable;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+
+/**
+ * A simple example program to use DataMonitor to start and
+ * stop executables based on a znode. The program watches the
+ * specified znode and saves the data that corresponds to the
+ * znode in the filesystem. It also starts the specified program
+ * with the specified arguments when the znode exists and kills
+ * the program if the znode goes away.
+ *
+ * This will act somewhat like a Controller from MVC.
+ */
+public class Executor implements Watcher, Runnable, DataMonitor.DataMonitorListener
+{
+	/**
+	 * Main Entry Point for program
+	 * @param args hostport znode logFile program [args ...]
+	 */
+	public static void main(String[] args) {
+
+		if (args.length < 4) {
+			System.err.println("USAGE: Executor hostPort znode logFile program [args ...]");
+			System.exit(2);
+		}
+
+		String hostPort = args[0];
+		String znode = args[1];
+		String filename = args[2];
+		String exec[] = new String[args.length - 3];
+		System.arraycopy(args, 3, exec, 0, exec.length);
+
+		try {
+			new Executor(hostPort, znode, filename, exec).run();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Simple class object that takes an input and output stream and
+	 * transfers the input to the output in a separate thread
+	 */
+	static class StreamWriter implements Runnable {
+		OutputStream os;
+		InputStream is;
+
+		StreamWriter(InputStream is, OutputStream os) {
+			this.is = is;
+			this.os = os;
+		}
+
+		@Override
+		public void run() {
+			byte b[] = new byte[80];
+			int rc;
+			try {
+				while ((rc = is.read(b)) > 0) {
+					os.write(b, 0, rc);
+				}
+			} catch (IOException e) {
+				System.out.println("Stream interruption captured in Stream Writer. Program context most likely switching...");
+			}
+		}
+	}
+
+	private DataMonitor dm;
+
+	private String logFile;
+	private String execution[];
+
+	private ExecutorService threadPool;
+	private Process child;
+
+	public Executor(String hostPort, String znode, String filename, String exec[])
+	throws KeeperException, IOException
+	{
+		this.logFile = filename;
+		this.execution = exec;
+		this.threadPool = Executors.newFixedThreadPool(4);
+
+		// Tell ZooKeeper to use the Executor class for all watch callbacks
+		// This will route the watch callbacks to the data monitor, which can only be created after
+		// the Zookeeper client. In order to break the circular dependency we'll tell ZooKeeper
+		// to use the Executor class for all watch callbacks. This will route the watch callbacks to ... etc etc etc
+		ZooKeeper zk = new ZooKeeper(hostPort, 3000, this);
+
+		dm = new DataMonitor(zk, znode, null, this);
+	}
+
+	@Override
+	public void run() {
+		try {
+			synchronized (this) {
+				// Event processing loop
+				while (!dm.isDead()) {
+					wait();
+				}
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * We do not process any events ourselves, we just need to forward them on.
+	 *
+	 * @see org.apache.zookeeper.Watcher#process(org.apache.zookeeper.WatchedEvent)
+	 */
+	@Override
+	public void process(WatchedEvent event) {
+		dm.process(event);
+	}
+
+	@Override
+	public void closing(int rc) {
+		synchronized (this) {
+			notifyAll();
+		}
+	}
+
+	/**
+	 * Carry out a task based on the data returned from the data monitor.
+	 *
+	 * @param data bytes of data contained in ZNode.
+	 */
+	@Override
+	public void processExistsStatusChangeEvent(@Nullable byte[] data) {
+		if (data == null) {
+
+			// Node does not exist
+
+			if (child != null) {
+				System.out.println("Killing process");
+				child.destroy();
+				try {
+					child.waitFor();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			child = null;
+
+		} else {
+
+			// Node Exists and has changed.
+
+			if (child != null) {
+				System.out.println("Stopping child");
+				child.destroy();
+				try {
+					child.waitFor();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+
+			// Log the znode data to a file.
+			try {
+				FileOutputStream fos = new FileOutputStream(logFile, true);
+				fos.write(data);
+				fos.write("\n".getBytes());
+				fos.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+			try {
+				System.out.println("Starting child");
+				child = Runtime.getRuntime().exec(execution);
+
+				// Use a set of threads to continuously pipe the child stdout streams to this process's stdout
+				threadPool.execute(new StreamWriter(child.getInputStream(), System.out));
+				threadPool.execute(new StreamWriter(child.getErrorStream(), System.err));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
